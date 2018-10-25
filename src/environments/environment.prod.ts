9878ca14e77562ddfda23e3d5356f30959e62c1f
Fredrick Otim <fredrickot@gmail.com>
2018-10-25 18:25:28 +0300

    Initial project setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyByrF2IkdyqyyYFuCEAngP2V-Vrte76-tg",
+        authDomain: "online-shop-c3107.firebaseapp.com",
+        databaseURL: "https://online-shop-c3107.firebaseio.com",
+        projectId: "online-shop-c3107",
+        storageBucket: "online-shop-c3107.appspot.com",
+        messagingSenderId: "373757994156"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyByrF2IkdyqyyYFuCEAngP2V-Vrte76-tg",
+        authDomain: "online-shop-c3107.firebaseapp.com",
+        databaseURL: "https://online-shop-c3107.firebaseio.com",
+        projectId: "online-shop-c3107",
+        storageBucket: "online-shop-c3107.appspot.com",
+        messagingSenderId: "373757994156"
+  }
 };
 
 /*
