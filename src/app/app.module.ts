8788603820227f2f7278e5ef2c2142421bb2c9cc
Fredrick Otim <fredrickot@gmail.com>
2018-10-26 09:32:40 +0300

    defines the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,34 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +39,16 @@ import { AppComponent } from './app.component';
     AngularFireModule.initializeApp(environment.firebase, 'online-shop'), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent }, 
+      { path: 'products', component: ProductsComponent }, 
+      { path: 'shopping-cart', component: ShoppingCartComponent }, 
+      { path: 'check-out', component: CheckOutComponent }, 
+      { path: 'order-success', component: OrderSuccessComponent }, 
+      { path: 'login', component: LoginComponent }, 
+      { path: 'admin/products', component: AdminProductsComponent }, 
+      { path: 'admin/orders', component: AdminOrdersComponent } 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
