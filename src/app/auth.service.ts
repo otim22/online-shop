a9b07f6104e75bb579e7df64362522c176d9936b
Fredrick Otim <fredrickot@gmail.com>
2018-10-26 13:14:08 +0300

    protests routes and redirects logging in

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router, ActivatedRoute } from '@angular/router';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
 
@@ -9,11 +10,14 @@ import { Observable } from 'rxjs';
 export class AuthService {
     user$: Observable<firebase.User>;
 
-    constructor(private afAuth: AngularFireAuth) { 
+    constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute, private router: Router) { 
         this.user$ = afAuth.authState;
     }
 
     login() {
+        let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+        localStorage.setItem('returnUrl', returnUrl);
+
         this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
     }
 
